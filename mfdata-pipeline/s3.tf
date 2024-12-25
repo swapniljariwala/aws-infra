de5d45aeb22c00ad1d4b7b29c7ce9f7a8cb6470d
Your Name <you@example.com>
2024-12-25 20:40:58 +0530

    destroyed mf infra

--- mfdata-pipeline/s3.tf
+++ mfdata-pipeline/s3.tf
@@ -15,6 +15,15 @@ resource "aws_s3_bucket" "lambda_code" {
 
   }
 }
+resource "aws_s3_bucket" "athena_results" {
+  bucket = "${local.prefix}-athena-results"
+
+  tags = {
+    Name        = "${local.prefix}-athena-results"
+
+  }
+}
+
 
 output "z_downloader_stock_data_bucket" {
   value = aws_s3_bucket.stock_data.bucket
@@ -24,13 +33,8 @@ output "z_downloader_code_bucket" {
     value = aws_s3_bucket.lambda_code.bucket
 }
 
-resource "aws_s3_bucket" "athena_results" {
-  bucket = "${local.prefix}-athena-results"
-
-  tags = {
-    Name        = "${local.prefix}-athena-results"
-
-  }
+output "z_downloader_athena_results_bucket" {
+    value = aws_s3_bucket.athena_results.bucket
 }
 
 
